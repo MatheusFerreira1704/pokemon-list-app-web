499fc1dbc576294799a167336b3b8df6fc561426
Matheus Ferreira da Silva <mattheus1704@gmail.com>
2021-03-12 16:19:38 -0300

    Antes da api

---
+++ pokemon-list/src/components/shared/AppTopBar.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="app-to-bar">
+    <v-app-bar
+      absolute
+      color="#fcb69f"
+      dark
+      shrink-on-scroll
+      src="https://s2.glbimg.com/TEcaFoBv0L84h7OcIp8b4Nm3pa0=/0x600/s.glbimg.com/po/tt2/f/original/2014/09/11/0330d49e012d959f123139180a6d.jpeg"
+      scroll-target="#scrolling-techniques-2"
+    >
+      <template> </template>
+      <v-app-bar-nav-icon @click="drawer = true"> </v-app-bar-nav-icon>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <!--Forme encontrada para baixar a imagem de inicio.-->
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="red darken-1
+"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Inicio</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-paw</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Pokemons</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppToBar",
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+};
+</script>
+
+<style scoped></style>>
---
+++ pokemon-list/src/components/shared/Inicio.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+    <h2 class="text-h5 text-center mb-3 mt-5">Lista de Pokemons</h2>
+
+    <v-img
+      class="inicio-imagem mb-4"
+      src="https://i2.wp.com/gamelogia.com.br/wp-content/uploads/2016/12/pokemon-segunda-gerac%CC%A7a%CC%83o.jpg?fit=1280%2C854&ssl=1"
+    >
+    </v-img>
+    <p class="text-justify">
+      Aqui nesse WEB-APP você encontrar informações sobre todos os Pokemons.
+      Fique a vontade para conhecer um pouco mais sobres os Pokemons.
+    </p>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Inicio",
+};
+</script>
+
+<style scoped>
+.inicio-imagem {
+  border-radius: 15px;
+}
+</style>
